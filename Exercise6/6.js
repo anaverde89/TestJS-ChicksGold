8e92a78cf611013f4c9df0dda1bccf2a9234838d
Ana Verde <anaverde89@gmail.com>
2022-01-27 17:58:15 -0400

    Exercise 6 (finish)

--- Exercise6/6.js
+++ Exercise6/6.js
@@ -1,35 +1,70 @@
-const humanizeNumber = (number) => {
-  let msg = number;
-  let lastNumber = parseInt(number.charAt(number.length - 1));
-  if (parseInt(number) > 10 && parseInt(number) < 14) {
-    return msg + "th";
+const range1 = (num1, num2) => {
+  let result = [];
+  if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
+      result.push(i);
+    }
   } else {
-    if (lastNumber >= 1 && lastNumber <= 3) {
-      switch (lastNumber) {
-        case 1:
-          msg += "st";
-          break;
-        case 2:
-          msg += "nd";
-          break;
-        case 3:
-          msg += "rd";
-          break;
-        default:
-          break;
-      }
-    } else {
-      msg += "th";
+    for (let i = num1; i >= num2; i--) {
+      result.push(i);
     }
   }
-  return msg;
+  return result;
+};
+
+const range2 = (num1, num2, step = 1) => {
+  let result = [];
+  if (num1 < num2) {
+    for (let i = num1; i <= num2; i += step) {
+      result.push(i);
+    }
+  } else {
+    for (let i = num1; i >= num2; i -= step) {
+      result.push(i);
+    }
+  }
+  return result;
+};
+
+const sumItemsArray = (arrayNums) => {
+  return arrayNums.reduce((num1, num2) => num1 + num2);
 };
 
 const clickButton = () => {
   let input = document.getElementById("inputNumber");
-  let result = humanizeNumber(input.value);
+  let input2 = document.getElementById("inputNumber2");
+  let number1 = input.value !== "" ? parseInt(input.value) : 0;
+  let number2 = input2.value !== "" ? parseInt(input2.value) : 0;
+  let result = range1(number1, number2);
   console.log(result);
 };
 
-let button = document.getElementById("buttonConvert");
+const clickButton2 = () => {
+  let input = document.getElementById("inputNumber3");
+  let input2 = document.getElementById("inputNumber4");
+  let input3 = document.getElementById("inputNumber5");
+  let number1 = input.value !== "" ? parseInt(input.value) : 0;
+  let number2 = input2.value !== "" ? parseInt(input2.value) : 0;
+
+  if (input3.value !== "") {
+    console.log(range2(number1, number2, parseInt(input3.value)));
+  } else {
+    console.log(range2(number1, number2));
+  }
+};
+
+const clickButton3 = () => {
+  let input = document.getElementById("inputNumber6");
+  let arrayInput =
+    input.value !== ""
+      ? input.value.split(",").map((index) => parseInt(index))
+      : [0];
+  console.log(sumItemsArray(arrayInput));
+};
+
+let button = document.getElementById("buttonCalculate");
 button.addEventListener("click", clickButton);
+let button2 = document.getElementById("buttonCalculate2");
+button2.addEventListener("click", clickButton2);
+let button3 = document.getElementById("buttonCalculate3");
+button3.addEventListener("click", clickButton3);
