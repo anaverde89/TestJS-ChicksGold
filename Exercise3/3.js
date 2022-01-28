c155537e18980c7aa40c3bc83e820a2111fd1e60
Ana Verde <anaverde89@gmail.com>
2022-01-27 22:55:39 -0400

    exercise 3 modified

--- Exercise3/3.js
+++ Exercise3/3.js
@@ -9,18 +9,38 @@ function countBs(text) {
       count++;
     }
   }
-  return count;
+  return `The number of "B" in the text is ${count}`;
 }
 
 function countChar(text, char) {
+  if (char == "") {
+    return "You must enter a character";
+  }
   let count = 0;
   for (let i = 0; i < text.length; i++) {
     if (text.charAt(i) === char) {
       count++;
     }
   }
-  return count;
+  return `The number of characters "${char}" in the text is ${count}`;
 }
 
-console.log(countBs("BBCbbbBBBmnb"));
-console.log(countChar("kakkerlakkkkkjkkkkjjjjjjjjjjjjjjjkj", "k"));
+const clickButton = () => {
+  let input = document.getElementById("inputText");
+  console.log(countBs(input.value));
+};
+
+const clickButton2 = () => {
+  let input = document.getElementById("inputText");
+  let text = input.value;
+  let input2 = document.getElementById("inputChar");
+  let char = input2.value;
+  console.log(countChar(text, char));
+};
+let button = document.getElementById("buttonCountB");
+button.addEventListener("click", clickButton);
+let button2 = document.getElementById("buttonCountChar");
+button2.addEventListener("click", clickButton2);
+
+// console.log(countBs("BBCbbbBBBmnb"));
+// console.log(countChar("kakkerlakkkkkjkkkkjjjjjjjjjjjjjjjkj", "k"));
